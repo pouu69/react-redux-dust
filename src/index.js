bcd45c3c8c68def8a20d2d94f908fb789192d671
KwanUng Park <pouu69@gmail.com>
2017-11-01 10:02:38 +0900

    delete: remove dev version

--- src/index.js
+++ src/index.js
@@ -1,30 +1,37 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
+// import { AppContainer } from 'react-hot-loader';
 import configureStore from './store';
 import Root from './containers/Root';
 
 const store = configureStore();
-
 render(
-  <AppContainer>
     <Root
       store={ store }
-    />
-  </AppContainer>,
+    />,
   document.getElementById('root')
 );
 
-if (module.hot) {
-  module.hot.accept('./containers/Root', () => {
-    const RootContainer = require('./containers/Root').default;
-    render(
-      <AppContainer>
-        <RootContainer
-          store={ store }
-        />
-      </AppContainer>,
-      document.getElementById('root')
-    );
-  });
-}
+
+// render(
+//   <AppContainer>
+//     <Root
+//       store={ store }
+//     />
+//   </AppContainer>,
+//   document.getElementById('root')
+// );
+
+// if (module.hot) {
+//   module.hot.accept('./containers/Root', () => {
+//     const RootContainer = require('./containers/Root').default;
+//     render(
+//       <AppContainer>
+//         <RootContainer
+//           store={ store }
+//         />
+//       </AppContainer>,
+//       document.getElementById('root')
+//     );
+//   });
+// }
--- src/store.js
+++ src/store.js
@@ -1,21 +1,20 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './module';
-import DevTools from './containers/DevTools';
+// import DevTools from './containers/DevTools';
 
-const enhancer = compose(
-  applyMiddleware(thunk),
-  DevTools.instrument()
-);
+// const enhancer = compose(
+//   applyMiddleware(thunk)
+// );
 
 export default function configureStore() {
-  const store = createStore(rootReducer, enhancer);
+  const store = createStore(rootReducer, applyMiddleware(thunk));
 
-  if (module.hot) {
-    module.hot.accept('./module', () =>
-      store.replaceReducer(require('./module').default)
-    );
-  }
+  // if (module.hot) {
+  //   module.hot.accept('./module', () =>
+  //     store.replaceReducer(require('./module').default)
+  //   );
+  // }
 
   return store;
 }
