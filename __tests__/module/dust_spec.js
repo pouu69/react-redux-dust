9f31d6d93be1fb538ece01f1836ce1b795fb438c
KwanUng Park <pouu69@gmail.com>
2017-11-02 10:17:43 +0900

    fix: add a comment

--- __tests__/module/dust_spec.js
+++ __tests__/module/dust_spec.js
@@ -30,25 +30,45 @@ describe('1. Redux Test', () => {
 
 	describe('1. Async Actions', () => {
 		let context = {
-			store : null
+			store : null,
+			action : null
 		}
 
 		beforeEach( () => {
-			context.store = mockStore(initState);
+			context = { 
+				store : mockStore(initState),
+				action: Actions.getDust
+			}
 		});
 
 
 		it('1-1. fetch를 시작하여 성공하면 "SUCCESS_FETCH_DUST" 액션이 생성됩니다. ', () => {
+			// Given
 			const region = 'seoul';
-			return context.store.dispatch(Actions.getDust(region)).then(() => {
+			const action = context.action(region);
+			const expected = {type: Actions.ActionTypes.SUCCESS_FETCH_DUST};
+
+			// When
+			const dispatch = context.store.dispatch(action);
+			return dispatch.then(() => {
+
+				// Then
 				const realActions = context.store.getActions();
-				expectChai(realActions[1]).to.deep.include({type: Actions.ActionTypes.SUCCESS_FETCH_DUST});
+				expectChai(realActions[1]).to.deep.include(expected);
 			});
 		});
 
 		it('1-2 fetch를 시작하여 실패하면 "FAILED_FETCH_DUST" 액션이 생성됩니다.', () => {
+			// Given
 			const region = 'abc';
-			return context.store.dispatch(Actions.getDust(region)).then(() => {
+			const action = context.action(region);
+			const expected = {type: Actions.ActionTypes.FAILED_FETCH_DUST};
+
+			// When
+			const dispatch = context.store.dispatch(action);
+			return dispatch.then(() => {
+
+				// Then
 				const realActions = context.store.getActions();
 				expectChai(realActions[1]).to.deep.include({type: Actions.ActionTypes.FAILED_FETCH_DUST});
 			});
@@ -56,13 +76,23 @@ describe('1. Redux Test', () => {
 	});
 
 	describe('2. Reducer', () => {
+		// Given
 		const reducers = Actions.default;
 
 		it('1. initState 반환', () => {
-			expect(reducers(undefined , {})).toEqual(initState);
+			// Given
+			const action = {};
+			const state = undefined;
+
+			// When
+			const result = reducers(state, action);
+
+			// Then
+			expect(result).toEqual(initState);
 		});
 
 		it('2. SUCCESS_FETCH_DUST 실행', () => {
+			// Given
 			const successFetchDustAction = {
 				type: Actions.ActionTypes.SUCCESS_FETCH_DUST,
 				payload
@@ -72,10 +102,17 @@ describe('1. Redux Test', () => {
 				progress: { isFetching: false },
 				dust: { val: 40, region: 'seoul', status: 'high' } 
 			}
-			expect(reducers({}, successFetchDustAction)).toEqual(expectResult); 
+
+			// When
+			const result = reducers({}, successFetchDustAction);
+
+
+			// Then
+			expect(result).toEqual(expectResult); 
 		});
 
 		it('3. FAILED_FETCH_DUST 실행', () => {
+			// Given
 			const failedFetchDustAction = {
 				type: Actions.ActionTypes.FAILED_FETCH_DUST,
 				payload: { message: "error" }
@@ -85,7 +122,12 @@ describe('1. Redux Test', () => {
 				failed: { message: "error" },
 				progress : { isFetching: false }
 			}
-			expect(reducers({}, failedFetchDustAction)).toEqual(expectResult); 
+
+			// When
+			const result = reducers({}, failedFetchDustAction);
+
+			// Then
+			expect(result).toEqual(expectResult); 
 		});
 	});
 });
